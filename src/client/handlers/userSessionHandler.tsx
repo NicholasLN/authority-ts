8d5e3e4a20161e471f10e04dd93a6d8794d3a497
PhilScott <PhilScott4USA@gmail.com>
2022-10-31 22:44:43 -0400

    add current character state, documentation.

--- src/client/handlers/userSessionHandler.tsx
+++ src/client/handlers/userSessionHandler.tsx
@@ -4,6 +4,7 @@ import Cookies from "js-cookie";
 import { login, logout, updateState } from "../redux/reducers/authSlice";
 import getPage from "../utils/getPage";
 import { BeatLoader } from "react-spinners";
+import { updateCharacters } from "../redux/reducers/characterSlice";
 
 type UserSessionHandlerProps = {
   children: JSX.Element;
@@ -13,26 +14,42 @@ function userSessionHandler(props: UserSessionHandlerProps) {
   const [loading, setLoading] = React.useState(true);
   const userState = useSelector((state: RootState) => state.auth);
   const isFocused = useSelector((state: RootState) => state.windowFocus.focus);
+  const characterState = useSelector((state: RootState) => state.character);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
     async function updateSession(): Promise<any> {
       var access_token = Cookies.get("access_token");
       if (!access_token) {
+        // No access token, so we're not logged in
         return Cookies.set("access_token", "null");
       }
+      // If the user is focused on the window, we'll update the session. Otherwise, we'll wait until they are.
+      // This is to prevent unnecessary requests to the server. Also, it'll refresh the state immediately once they come back.
       if (isFocused) {
-        try {
-          var resp = await getPage("/api/user/profile", access_token);
-          if (resp.data) {
-            if (!userState.loggedIn) {
-              dispatch(login(resp.data));
+        // If the user has an access token, we'll check/update the session (if it's valid and not expired/malformed)
+        if (access_token != "null") {
+          try {
+            var resp = await getPage("/api/user/profile", access_token);
+            // If the response is 200, we'll update the state with the new session
+            if (resp.data) {
+              // Restore the session if the state is empty (i.e. the user has refreshed the page or revisited the site after a while)
+              if (!userState.loggedIn) {
+                dispatch(login(resp.data));
+              }
+              // Otherwise, if they already have an active state, we'll update it to reflect the users new information
+              dispatch(updateState(resp.data));
+              // Update characters as well
+              dispatch(updateCharacters(resp.data.characters));
             }
-            dispatch(updateState(resp.data));
+          } catch (e: any) {
+            // If there's any errors at all, we'll log the user out and prompt them to log back in.
+            // Possible errors include:
+            // 1. The access token is expired, malformed, or invalid
+            // 2. The user has been deleted
+            // 3. And other errors that may occur
+            dispatch(logout());
           }
-        } catch (e: any) {
-          Cookies.set("access_token", "null");
-          dispatch(logout());
         }
       }
       setLoading(false);
