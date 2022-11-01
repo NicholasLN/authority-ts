2a32382db2eaa27af386b391f360e9ae78a73456
PhilScott <PhilScott4USA@gmail.com>
2022-10-31 23:31:15 -0400

    hide sidebar by default

--- src/client/redux/reducers/uiSlice.ts
+++ src/client/redux/reducers/uiSlice.ts
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const uiSlice = createSlice({
   name: "ui",
   initialState: {
-    hideSidebar: false,
+    hideSidebar: true,
   },
   reducers: {
     toggleSidebar: (state) => {
