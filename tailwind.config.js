6a65f510d2ee7f6923f408c3e15dbee4be024dfe
PhilScott <PhilScott4USA@gmail.com>
2022-11-03 01:28:20 -0400

    half padding option

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      padding: {
+        0.5: "0.125rem",
+      },
       screens: {
         xs: "320px",
       },
