88b4dddc2d144d243bd1212720257b8c50e6157e
PhilScott <PhilScott4USA@gmail.com>
2022-11-13 14:33:37 -0500

    region model included

--- src/server/mongo/models/index.ts
+++ src/server/mongo/models/index.ts
@@ -3,6 +3,7 @@ import { logDB } from "../../utils/logging";
 // This file is just a collection of all the models in the project so that the database can be initialized with all of them
 require("../models/User");
 require("../models/Character");
+require("../models/Region");
 
 function main() {
   logDB("Models initialized!", true);
