3058a57d0a1cd51aaaa32cff14dffcb794e100e8
PhilScott <PhilScott4USA@gmail.com>
2022-11-28 13:14:04 -0500

    reorder

--- src/server/mongo/models/index.ts
+++ src/server/mongo/models/index.ts
@@ -3,9 +3,9 @@ import { logDB } from "../../utils/logging";
 import initGovernmentModels from "./government";
 
 // This file is just a collection of all the models in the project so that the database can be initialized with all of them
+require("../models/Region");
 require("../models/User");
 require("../models/Character");
-require("../models/Region");
 initGovernmentModels();
 
 function main() {
