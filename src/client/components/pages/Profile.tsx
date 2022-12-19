5d4d3699cc63a36397ba09dddd73f21d1cc7ff7f
PhilScott <PhilScott4USA@gmail.com>
2022-12-19 11:08:19 -0500

    placeholder

--- src/client/components/pages/Profile.tsx
+++ src/client/components/pages/Profile.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux/es/exports";
 import { useNavigate } from "react-router";
+import CreateCharacterModal from "../modals/CreateCharacterModal";
 import SelectCharacterModal from "../modals/SelectCharacterModal";
 import Body from "../struct/Body";
 import Selectable from "../struct/Selectable";
@@ -19,13 +20,22 @@ export default function Profile() {
   return (
     <Body>
       <p className="text-3xl">Welcome {userState.user.username}</p>
-      <SelectCharacterModal
-        onClose={() => {
-          navigate(`/character?id=${charState.currentCharacter._id}`);
-        }}
-        shown={true}
-        chars={charState.characters}
-      />
+      {charState.characters.length > 0 ? (
+        <SelectCharacterModal
+          onClose={() => {
+            navigate(`/character?id=${charState.currentCharacter._id}`);
+          }}
+          shown={true}
+          chars={charState.characters}
+        />
+      ) : (
+        <CreateCharacterModal
+          onClose={() => {
+            navigate(`/character?id=${charState.currentCharacter._id}`);
+          }}
+          shown={true}
+        />
+      )}
     </Body>
   );
 }
