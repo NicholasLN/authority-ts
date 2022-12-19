d05f6d4a0475217e5848dc6fddb5a45cfd703255
PhilScott <PhilScott4USA@gmail.com>
2022-12-19 11:09:33 -0500

    updated navbar to show country

--- src/client/components/struct/Navbar.tsx
+++ src/client/components/struct/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
@@ -11,12 +11,16 @@ import {
 export default function Navbar() {
   const [navbarDropdownsShown, setNavbarDropdownsShown] = React.useState({
     user: false,
-    characters: false,
+    country: false,
   });
 
   const userState = useSelector((state: RootState) => state.auth);
   const charState = useSelector((state: RootState) => state.character);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(charState, userState);
+  });
 
   const logoutFunc = function (): any {
     dispatch(logout());
@@ -45,13 +49,13 @@ export default function Navbar() {
           </>
         ) : (
           <>
-            {/* If the user is logged in, display their username and a dropdown menu with a link to their profile and a logout button */}
-            <div className="relative">
+            {/* User dropdown. */}
+            <div className="relative ml-2">
               <button
                 onClick={() =>
                   setNavbarDropdownsShown({
-                    ...navbarDropdownsShown,
                     user: !navbarDropdownsShown.user,
+                    country: false,
                   })
                 }
                 className="flex flex-row items-center px-2 py-1 rounded-md bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
@@ -96,13 +100,52 @@ export default function Navbar() {
                 </div>
               </div>
             </div>
-            <div>
-              <Link to="/map">
-                <button className="px-2 py-1 rounded-md bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50">
-                  Map
+            {/* Country dropdown */}
+            {charState.currentCharacter && (
+              <div className="relative ml-2">
+                <button
+                  onClick={() =>
+                    // set all dropdowns to false, then set the country dropdown to true
+                    setNavbarDropdownsShown({
+                      user: false,
+                      country: !navbarDropdownsShown.country,
+                    })
+                  }
+                  className="flex flex-row items-center px-2 py-1 rounded-md bg-gray-700 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
+                >
+                  <span className="text-sm font-medium">
+                    <p>{charState.currentCharacter.country.name}</p>
+                  </span>
+                  <svg
+                    className="w-5 h-5 ml-2 -mr-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm0 4a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 4a1 1 0 100 2h6a1 1 0 100-2H6z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </button>
-              </Link>
-            </div>
+                <div
+                  className={`${
+                    navbarDropdownsShown.country ? "block" : "hidden"
+                  } absolute right-0 w-48 mt-2 z-50 origin-top-right bg-gray-700 border border-gray-600 divide-y divide-gray-600 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                >
+                  <div className="px-1 py-1 ">
+                    <Link
+                      to={`/country/${charState.currentCharacter._id}`}
+                      className="block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600"
+                    >
+                      {charState.currentCharacter.name}
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            )}
           </>
         )}
       </div>
