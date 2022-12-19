40e482a85462d69a8b74a7341980617b92afb7e4
PhilScott <PhilScott4USA@gmail.com>
2022-12-19 11:09:39 -0500

    404 page

--- src/client/router/router.tsx
+++ src/client/router/router.tsx
@@ -11,6 +11,8 @@ import Register from "../components/pages/Register";
 import Profile from "../components/pages/Profile";
 import Character from "../components/pages/Character";
 import Map from "../components/map/Map";
+import Country from "../components/pages/Country";
+import NotFound from "../components/struct/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,11 +22,24 @@ const router = createBrowserRouter(
       <Route path="register" element={<Register />} />
       <Route path="profile" element={<Profile />} />
       <Route path="map" element={<Map />} />
+
+      {/* Country */}
+      <Route path="country">
+        <Route path="" element={<Country />} />
+        <Route path=":/" element={<Country />} />
+      </Route>
+
       {/* Character Paths */}
       <Route path="character">
         <Route path="" element={<Character />} />
         <Route path=":id/" element={<Character />} />
       </Route>
+
+      {/* 404 */}
+      <Route
+        path="*"
+        element={<NotFound errorType="404" errorMessage="Page not found." />}
+      />
     </Route>
   )
 );
