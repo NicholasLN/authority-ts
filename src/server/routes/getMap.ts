fb1797acf0442c32e3f51e0b5d294baedf5f1cf7
PhilScott <PhilScott4USA@gmail.com>
2022-12-19 11:10:58 -0500

    getMap

--- src/server/routes/getMap.ts
+++ src/server/routes/getMap.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import Region from "../mongo/models/Region";
+import Country from "../mongo/models/government/Country";
 var router = express.Router();
 
 router.get("/", async (req, res) => {
   var geoJSON = [] as any;
   var allRegions = await Region.find({});
-  allRegions.map((region) => {
-    geoJSON.push(region.returnAsGeoJSON());
-  });
+  await Promise.all(
+    allRegions.map(async (region) => {
+      var country = await Country.findOne({ regions: region._id });
+      var regionGeoJSON = region.returnAsGeoJSON();
+      if (country) {
+        const { name, color } = country;
+        regionGeoJSON.properties.country = name;
+        regionGeoJSON.properties.color = color;
+      }
+      geoJSON.push(regionGeoJSON);
+    })
+  );
   res.send(geoJSON);
 });
 
