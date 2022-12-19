e2348312b02a6ef034b7bd7251673b13cfa66d92
PhilScott <PhilScott4USA@gmail.com>
2022-12-19 11:10:41 -0500

    remove character from global types to fix conflict

--- src/server/@types/index.t.ts
+++ src/server/@types/index.t.ts
@@ -7,21 +7,6 @@ declare global {
     inclusive: boolean
   ): boolean;
   function generateJWT(object: any, remember: boolean): string;
-  interface Character {
-    name: string;
-    user: string;
-    picture: string;
-    age: number;
-    gender: string;
-    location: string;
-    personalityStats: {
-      rhetoric: number;
-      intelligence: number;
-      charisma: number;
-      dealmaking: number;
-      leadership: number;
-    };
-  }
 }
 
 export {};
